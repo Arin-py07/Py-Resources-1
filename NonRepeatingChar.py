96ff059975322f7136f26a4f69d91871ac451e1a
Arindam Dey <deyarindam475@gmail.com>
2021-06-21 21:48:04 +0530

    Create NonRepeatingChar.py

---
+++ NonRepeatingChar.py
@@ -0,0 +1,21 @@
+Input:
+S = hello
+Output: h
+  
+Solution:
+  
+from collections import Counter 
+class Solution:
+    
+   #Function to find the first non-repeating character in a string.
+    def nonrepeatingCharacter(self,s):
+        #code here
+        freq = Counter(s)
+        
+        for i in s:
+          
+            if (freq[i] == 1):
+                return i
+                break
+                
+        return -1
